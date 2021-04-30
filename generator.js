13d596eab2c65d73a2ebef2a31c73b4228bd2cae
Caleb Johnstone <u19030119@tuks.co.za>
2021-04-30 12:54:05 +0200

    testing repositories are seperate

--- generator.js
+++ generator.js
@@ -129,8 +129,7 @@ document.getElementById('inputFileText').addEventListener('keydown', function(e)
       var end = this.selectionEnd;
   
       // set textarea value to: text before caret + tab + text after caret
-      this.value = this.value.substring(0, start) +
-        " " + this.value.substring(end);
+      this.value = this.value.substring(0, start) + " " + this.value.substring(end);
   
       // put caret at right position again
       this.selectionStart =
